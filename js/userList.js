f05ca8c905ec0ae6abc48fcc280f42206ad6d973
sinslav <sinslav@gmail.com>
2015-05-22 15:16:51 +0300

    new file:   test.js

---
+++ js/userList.js
@@ -0,0 +1,38 @@
+var users = [
+	{	id: 0,
+		firstname: "Yaroslav",
+		lastname: "Sinchenko",
+		photo: "img/0.jpg"
+	},
+	{	id: 1,
+		firstname: "Anatoliy";
+		lastname: "Zalusky",
+		photo: "img/1.jpg"
+	},
+		{id: 2,
+		firstname: "Inna",
+		lastname: "Ulking",
+		photo: "img/2.jpg"
+	},
+	{	id: 3,
+		firstname: "Elmend",
+		lastname: "Bzigovsky",
+		photo: "img/3.jpg"
+	},
+	{	id: 4,
+		firstname: "Umora",
+		lastname: "Robinzon",
+		photo: "img/4.jpg"
+	},
+	{	id: 5,
+		firstname: "Veskold",
+		lastname: "Yellopukky",
+		photo: "img/5.jpg"
+	},
+		{	id: 6,
+		firstname: "Sidora",
+		lastname: "Partenit",
+		photo: "img/6.jpg"
+	},
+	
+];
